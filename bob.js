13380a55fa4b07923ddb1cb5b48cc3b5933de4e2
Atulya630 <76642045+Atulya630@users.noreply.github.com>
2021-10-05 20:30:54 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,34 @@
+class bob
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic: false,
+			'restituion':1.4,
+			'friction':1.0,
+			'density':1.0
+			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var paperpos=this.body.position;
+			push()
+			translate(paperpos.x, paperpos.y);
+			rectMode(CENTER)
+			fill(255,0,255)
+			ellipse(0,0,this.r, this.r);
+			pop()
+			
+	}
+
+}
+
